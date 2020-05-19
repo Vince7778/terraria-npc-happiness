46aaeaf688119c3af2c16a330e149249cc7b6fb5
Vince7778 <realvince7778@gmail.com>
2020-05-19 12:40:05 -0700

    Add Biome component
    Created a new Biome component and added the Underground.

--- main.js
+++ main.js
@@ -1,3 +1,34 @@
+
+const names = {
+    underground: "Underground"
+}
+
+const biomeProps = {
+    underground: {
+        colors: {
+            border: "secondary",
+            text: "white",
+            bg: "secondary",
+        },
+        name: "Underground",
+    },
+}
+
+class Biome extends React.Component {
+    render() {
+        const biomeProp = biomeProps[this.props.biomeType];
+        const colorType = biomeProp.colors;
+        const divClass = "p-3 rounded-lg bg-"+colorType.bg+" text-"+colorType.text+" border-"+colorType.border;
+        const biomeName = biomeProp.name;
+
+        return (
+            <div class={divClass}>
+                <h4>{biomeName}</h4>
+            </div>
+        );
+    }
+}
+
 class Tool extends React.Component {
     constructor(props) {
         super(props);
@@ -6,7 +37,7 @@ class Tool extends React.Component {
 
     render() {
         return (
-            <p>Hello</p>
+            <Biome biomeType="underground" />
         );
     }
 }
