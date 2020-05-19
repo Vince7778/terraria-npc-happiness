e802a92da0f4d231f99a03b115ace32167e22e3a
Vince7778 <realvince7778@gmail.com>
2020-05-19 14:06:56 -0700

    Add ability to add multiple biomes
    Multiple biomes are now able to be added to the Tool instance. This
    needs to be done to allow custom configurations of biomes.

--- main.js
+++ main.js
@@ -1,12 +1,8 @@
 
-const names = {
-    underground: "Underground"
-}
-
 const biomeProps = {
     underground: {
         colors: {
-            border: "secondary",
+            border: "dark",
             text: "white",
             bg: "secondary",
         },
@@ -17,8 +13,11 @@ const biomeProps = {
 class Biome extends React.Component {
     render() {
         const biomeProp = biomeProps[this.props.biomeType];
+
         const colorType = biomeProp.colors;
-        const divClass = "p-3 rounded-lg bg-"+colorType.bg+" text-"+colorType.text+" border-"+colorType.border;
+        let divClass = "p-3 rounded-lg bg-"+colorType.bg+" text-"+colorType.text+" border border-"+colorType.border;
+        if (this.props.needsMargin) divClass += " mt-2";
+
         const biomeName = biomeProp.name;
 
         return (
@@ -33,13 +32,34 @@ class Tool extends React.Component {
     constructor(props) {
         super(props);
         this.state = { biomes: [] }
+        this.addBiome = this.addBiome.bind(this);
+    }
+
+    addBiome(type) {
+        const biomes = this.state.biomes;
+
+        this.setState({
+            biomes: biomes.concat({
+                type: type,
+            })
+        });
     }
 
     render() {
+        const biomeState = this.state.biomes;
+        const biomeList = biomeState.map((biome, ind) => {
+            const needsMargin = ind >= 1;
+            return (
+                <Biome biomeType={biome.type} needsMargin={needsMargin}/>
+            )
+        });
+
         return (
-            <Biome biomeType="underground" />
+            <div>{biomeList}</div>
         );
     }
 }
 
-ReactDOM.render(<Tool />, document.getElementById("tool"))
+const tool = ReactDOM.render(<Tool />, document.getElementById("tool"));
+tool.addBiome("underground");
+tool.addBiome("underground");
