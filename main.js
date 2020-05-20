44c9e775b75d0087718f92316d1a63e61d449fbe
Vince7778 <realvince7778@gmail.com>
2020-05-20 11:33:03 -0700

    Add ability to move NPCs between biomes
    Adds the up and down arrows before the NPC name to move the NPC. Before
    this change, it was impossible to get an NPC in the biome you want.

--- main.js
+++ main.js
@@ -12,8 +12,19 @@ class NPC extends React.Component {
         
         const npcName = npcProps[npcType].name;
 
+        const upBtn = this.props.isFirstBiome ? null : 
+            (<button type="button" className="btn btn-outline-secondary btn-sm" onClick={this.props.moveNPCUp}><i className="fas fa-chevron-up"></i></button>);
+        const downBtn = this.props.isLastBiome ? null : 
+            (<button type="button" className="btn btn-outline-secondary btn-sm" onClick={this.props.moveNPCDown}><i className="fas fa-chevron-down"></i></button>);
+
         return (
             <div className={className}>
+                <div className="col-auto">
+                    <div className="btn-group-vertical">
+                        {upBtn}
+                        {downBtn}
+                    </div>
+                </div>
                 <div className="col-auto">
                     <img src={imgSrc}></img>
                 </div>
@@ -34,7 +45,7 @@ class Biome extends React.Component {
         const biomeProp = biomeProps[biomeType];
 
         let divClass = "row mx-0 p-3 rounded-lg " + biomeType + " border border-"+biomeType;
-        if (this.props.needsMargin) divClass += " mt-2";
+        if (!this.props.isFirst) divClass += " mt-2";
 
         const btnClass = "btn btn-"+biomeType;
 
@@ -42,7 +53,16 @@ class Biome extends React.Component {
 
         const npcList = this.props.npcs.map((npc, ind) => {
             return (
-                <NPC key={ind} npcType={npc} ind={ind} removeNPC={() => {this.props.removeNPC(npc)}}/>
+                <NPC 
+                    key={ind} 
+                    npcType={npc} 
+                    ind={ind} 
+                    removeNPC={() => {this.props.removeNPC(npc)}}
+                    isFirstBiome={this.props.isFirst}
+                    isLastBiome={this.props.isLast}
+                    moveNPCUp={() => this.props.moveNPCUp(npc)}
+                    moveNPCDown={() => this.props.moveNPCDown(npc)}
+                />
             );
         });
 
@@ -67,7 +87,7 @@ class Biome extends React.Component {
 class Tool extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { biomes: [], idInd: 0 }
+        this.state = { biomes: [] }
         this.addBiome = this.addBiome.bind(this);
     }
 
@@ -76,43 +96,28 @@ class Tool extends React.Component {
         const id = this.state.idInd;
         const biomesConcat = biomes.concat({
             type: type,
-            id: id,
             npcs: [],
         });
 
         biomesConcat.sort((a, b) => {
             return (biomeProps[a.type].order - biomeProps[b.type].order);
-        })
+        });
 
         this.setState({
             biomes: biomesConcat,
-            idInd: id+1,
         });
     }
 
-    deleteBiome(id) {
+    deleteBiome(ind) {
         const biomes = this.state.biomes.slice();
-        let biomeInd;
-        biomes.forEach((biome, ind) => {
-            if (id == biome.id) {
-                biomeInd = ind;
-            }
-        });
-        const removed = biomes.splice(biomeInd, 1);
+        const removed = biomes.splice(ind, 1);
 
         removed[0].npcs.forEach(type => {
-            const dropdown = document.getElementById("npcDropdownMenu");
-            const children = dropdown.children;
-            for (let i = 0; i < children.length; i++) {
-                if (children[i].innerHTML === npcProps[type].name) {
-                    children[i].className = "dropdown-item";
-                }
-            }
+            toggleNPCDropdown(type, false);
         });
 
         this.setState({
             biomes: biomes,
-            id: this.state.id,
         });
     }
 
@@ -139,17 +144,10 @@ class Tool extends React.Component {
 
             biomes[prefInd].npcs.push(type);
 
-            const dropdown = document.getElementById("npcDropdownMenu");
-            const children = dropdown.children;
-            for (let i = 0; i < children.length; i++) {
-                if (children[i].innerHTML === npcProps[type].name) {
-                    children[i].className = "dropdown-item disabled";
-                }
-            }
+            toggleNPCDropdown(type, true);
 
             this.setState({
                 biomes: biomes,
-                idInd: this.state.idInd,
             });
         }
     }
@@ -165,32 +163,55 @@ class Tool extends React.Component {
             }
         }
 
-        const dropdown = document.getElementById("npcDropdownMenu");
-        const children = dropdown.children;
-        for (let i = 0; i < children.length; i++) {
-            if (children[i].innerHTML === npcProps[type].name) {
-                children[i].className = "dropdown-item";
-            }
-        }
+        toggleNPCDropdown(type, false);
 
         this.setState({
             biomes: biomes,
-            idInd: this.state.idInd,
+        });
+    }
+
+    moveNPCUp(type, biomeInd) {
+        const biomes = this.state.biomes.slice();
+        if (biomeInd >= 1) {
+            const ind = biomes[biomeInd].npcs.indexOf(type);
+            if (ind != -1) {
+                biomes[biomeInd].npcs.splice(ind, 1);
+                biomes[biomeInd-1].npcs.push(type);
+            }
+        }
+        this.setState({
+            biomes: biomes,
+        });
+    }
+
+    moveNPCDown(type, biomeInd) {
+        const biomes = this.state.biomes.slice();
+        if (biomeInd < biomes.length - 1) {
+            const ind = biomes[biomeInd].npcs.indexOf(type);
+            if (ind != -1) {
+                biomes[biomeInd].npcs.splice(ind, 1);
+                biomes[biomeInd+1].npcs.unshift(type);
+            }
+        }
+        this.setState({
+            biomes: biomes,
         });
     }
 
     render() {
         const biomeState = this.state.biomes;
         const biomeList = biomeState.map((biome, ind) => {
-            const needsMargin = ind >= 1;
             return (
                 <Biome
-                    key={biome.id} 
+                    key={ind} 
                     biomeType={biome.type} 
-                    needsMargin={needsMargin} 
-                    delete={() => this.deleteBiome(biome.id)} 
+                    isFirst={ind == 0} 
+                    isLast={ind == biomeState.length-1}
+                    delete={() => this.deleteBiome(ind)} 
                     npcs={biome.npcs} 
-                    removeNPC={type => this.removeNPC(type)}
+                    removeNPC={type => this.removeNPC(type, ind)}
+                    moveNPCUp={type => this.moveNPCUp(type, ind)}
+                    moveNPCDown={type => this.moveNPCDown(type, ind)}
                 />
             );
         });
@@ -245,4 +266,15 @@ function addAllNPCs() {
     npcList.forEach(npcName => {
         tool.addNPC(npcName);
     });
+}
+
+function toggleNPCDropdown(type, disabled) {
+    const dropdown = document.getElementById("npcDropdownMenu");
+    const children = dropdown.children;
+    for (let i = 0; i < children.length; i++) {
+        if (children[i].innerHTML === npcProps[type].name) {
+            children[i].className = "dropdown-item";
+            if (disabled) children[i].className += " disabled";
+        }
+    }
 }
